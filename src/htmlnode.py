90d2ef28c85a337446a4dcdf03edb1825a00eb8d
Regg44 <kendall.av04@gmail.com>
2024-12-10 16:40:57 -0600

    Added ParentNode class and testing scripts, an easier way to test must be found

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -15,9 +15,34 @@ class HTMLNode:
     def props_to_html(self):
         if isinstance(self.props, dict):
             nsentences = list(map(lambda x: f'{x}="{self.props[x]}"', self.props))
-            return " ".join(nsentences)
-        raise Exception("Invalid format")
+            return " " + " ".join(nsentences)
+        if self.props == None:
+            return ""
     
     #When printed, this will execute and show if no other thing is called.
     def __repr__(self):
         return f"tag={self.tag}, value={self.value}, children={self.children}, props={self.props}"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag=tag, value=value, props=props, children=None)
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("All leaf notes must have a value")
+        if self.tag == None:
+            return self.value
+        else:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, value=None, children=children, props=props)
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("No tag present, cannot proceed")
+        if self.children == None:
+            raise ValueError("ParentNode must have children")
+        else:
+            line_so_far = "".join(list(map(lambda x: x.to_html(), self.children)))
+            return f"<{self.tag}{self.props_to_html()}>{line_so_far}</{self.tag}>"
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -9,7 +9,31 @@ class htmltest(unittest.TestCase):
         html1 = HTMLNode()
         self.assertEqual("tag=None, value=None, children=None, props=None", str(html1))
 
+class leaftest(unittest.TestCase):
+    def test1(self):
+        obj1 = LeafNode("a", "vamo pa la fiesta mi rico", {"href": "www.boot.com"})
+        print(obj1.to_html())
+        self.assertIsInstance(obj1.to_html(), str) 
+    
+    def test2(self):
+        obj1 = LeafNode("p", "This is an example test to do tests and stuff alike")
+        print(obj1.to_html())
+        self.assertIsInstance(obj1.to_html(), str)
 
+class parenttest(unittest.TestCase):
+    node = ParentNode(
+    "p",
+    children=[
+        LeafNode("b", "Bold text"),
+        LeafNode(None, "Normal text"),
+        LeafNode("i", "italic text"),
+        LeafNode(None, "Normal text"),
+    ],
+    )
+
+    node2 = ParentNode("h1", children=[node, LeafNode("b", "bald")],)
+    print(node.to_html())
+    print(node2.to_html())
 
 if __name__ == "__main__":
     unittest.main()
